f6ed59baed0635c53a8a4a04f4e30d99fb7350b0
hasamahas <hasanshaik3@gmail.com>
2025-04-27 23:19:58 +0530

    test

--- 01-mysql.sh
+++ 01-mysql.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -13,12 +14,11 @@ Y="\e[33m"
 echo "Enter MySQL db password:"
 read -s mysql_root_password
 VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
-        echo -e "$2...$R FAILURE $N"
-        exit 1
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2..$R FAILURE $N"
     else
-        echo -e "$2...$G SUCCESS $N"
+        echo -e "$2..$G SUCCESS $N"
     fi
 }
 if [ $USERID -ne 0 ]
